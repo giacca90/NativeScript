eed3e7b84fb960288b7c330a6cff0737cb141bb2
Giacca90 <giacca90mobile@gmail.com>
2023-08-14 22:27:25 +0200

    Usar la cámara

--- my-drawer-ng/src/app/featured/featured.component.ts
+++ my-drawer-ng/src/app/featured/featured.component.ts
@@ -3,6 +3,9 @@ import { RadSideDrawer } from 'nativescript-ui-sidedrawer'
 import { Application, Color, View } from '@nativescript/core'
 import * as SocialShare from "@nativescript/social-share";
 import { ImageSource } from "@nativescript/core";
+import { ToastDuration, Toasty } from "@triniwiz/nativescript-toasty";
+import * as camera from "@nativescript/camera";
+import * as ism from "@nativescript/core/image-source"
 
 @Component({
   selector: 'Featured',
@@ -79,5 +82,37 @@ let image: ImageSource =ImageSource.fromFileOrResourceSync("res://stop_app");
 
      SocialShare.shareImage(image);
 //    SocialShare.shareImage(image);
+  }
+
+  prueba5()  {
+    const options = {
+      width: 300,
+      height: 300,
+      keepAspectRatio: false,
+      saveToGallery: true
+  };
+    camera.requestCameraPermissions().then(
+      function success() {     
+        console.log("acceso conseguido!!!");
+       camera.takePicture(options)
+      .then((imageAsset) => {
+          console.log("Size: " + imageAsset.options.width + "x" + imageAsset.options.height);
+          console.log("keepAspectRatio: " + imageAsset.options.keepAspectRatio);
+          console.log("Photo saved in Photos/Gallery for Android or in Camera Roll for iOS");
+          ism.fromAsset(imageAsset).then((imageSource) => 
+          SocialShare.shareImage(imageSource)
+          ).catch((err) => console.log("Error => "+err));
+          
+      }).catch((err) => {
+          console.log("Error -> " + err.message);
+      });
+    },
+      function failure() {
+        const toast = new Toasty({ text: "Error al acceder a la Camara!!!" }).setToastDuration(
+          ToastDuration.SHORT
+        );
+        toast.show();
+      }
+  );
   }
 }
