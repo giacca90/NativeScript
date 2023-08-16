6ca85d6610ea2f3de22e95cfdd3cfd6deffe1f4b
Giacca90 <giacca90mobile@gmail.com>
2023-08-16 15:04:30 +0200

    Karma server y jUnit reporter

--- my-drawer-ng/karma.conf.js
+++ my-drawer-ng/karma.conf.js
@@ -28,7 +28,18 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'junit'],
+ // the default configuration
+    junitReporter: {
+      outputDir: '', // results will be saved as $outputDir/$browserName.xml
+      outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+      suite: '', // suite will become the package name attribute in xml testsuite element
+      useBrowserName: true, // add browser name to report and classes names
+      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+      properties: {}, // key value pair of properties to add to the <properties> section of the report
+      xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+},  
 
     // configure optional coverage, enable via --env.codeCoverage
     coverageReporter: {
